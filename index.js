5d1578bcb6535cd5375715fc0b844ea30faa7538
KelvinNaikuni <kelvin.naikuni@student.moringaschool.com>
2023-09-21 06:58:15 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2; 
+const num3 = 12;
+const num4 = 8;
+const mod = num3 % num4;
+const random = Math.floor(Math.random() * 10) + 1;
+const max = Math.max(16, 17, 18, 19, 20);
 
-// Write your code here
+console.log("multiply:", multiply);
+console.log("random:", random);
+console.log("mod:", mod);
+console.log("max:", max);
